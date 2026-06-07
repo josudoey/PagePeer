ead94ea0005fb53cb2b78178fb1b6ed69298ffa9
Joey Fang <josudoey@gmail.com>
2026-06-07 19:20:40 +0800

    feat(project): initialize astro project with react, tailwind css, and prettier

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ...require('prettier-config-standard'),
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro'
+      }
+    }
+  ]
+}
